04ac782dc4de01c35959d285cd8939b82ec31e4b
SolomonSan11 <darthvedar11@gmail.com>
2022-04-18 13:54:57 +0630

    [modify] 'enroll' table in frontend

--- resources/views/backend/enrollment/index.blade.php
+++ resources/views/backend/enrollment/index.blade.php
@@ -50,72 +50,93 @@
 
 </section>
 
-<!-- End Navbar -->
-<div class="container-fluid py-2" style="overflow-x: hidden;">
-    <div class="row">
-    <div class="col-10 offset-10">
 
-    </div>
-    </div>
+      <div class="container-fluid py-4">
       <div class="row">
-         <div class="col-12">
-          <div class="card my-2" >
-              <div class="container">
+        <div class="col-12">
+          <div class="card my-4">
 
-             <div class="row">
-        @foreach ($enrolls as $enroll)
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
 
-        <div class="col-md-6 mt-4">
-          <div class="card">
-            <div class="card-body pt-4 p-3">
-              <ul class="list-group">
-                <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
-                  <div class="d-flex flex-column">
-                    <h6 class="mb-3 text-sm">{{ \Carbon\Carbon::parse($enroll->created_at)->diffForHumans() }}</h6>
-                    <span class="mb-2 text-xs">User Name: <span class="text-dark font-weight-bold ms-sm-2">{{$enroll -> name}}</span></span>
-                    <span class="mb-2 text-xs">Course Title: <span class="text-dark font-weight-bold ms-sm-2">{{$enroll -> title}}</span></span>
-                    <span class="mb-2 text-xs">Bank Name: <span class="text-dark font-weight-bold ms-sm-2">{{$enroll -> bank_name}}</span></span>
-                    <span class="mb-2 text-xs">Course Fee: <span class="text-dark font-weight-bold ms-sm-2">{{$enroll -> amount}}</span></span>
-                    <span class="mb-2 text-xs">Enrollment Status: <span class="text-dark ms-sm-2 font-weight-bold">
+              <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                <h6 class="text-white text-capitalize ps-3">Student's Enrollment</h6>
+                <!-- <a class="btn btn-link text-dark px-3 mb-0" href="{{ route('export') }}" method="POST" enctype="multipart/form-data"><i class="material-icons text-sm me-2">pdf</i>Excel</a> -->
+              </div>
+            </div>
+            <div class="card-body px-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                  <!-- <thead>
+                    <tr>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Author</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Function</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Employed</th>
+                      <th class="text-secondary opacity-7"></th>
+                    </tr>
+                  </thead> -->
+                  <tbody>
+                  @foreach($enrolls as $enroll)
+                    <tr>
+                    <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold">{{$enroll->created_at}}</span>
+                      </td>
+                      
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm">{{ $enroll->name}}</h6>
+                            <p class="text-xs text-secondary mb-0">{{$enroll->title}}</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <p class="text-xs font-weight-bold mb-0">{{$enroll->bank_name}}</p>
+                        <p class="text-xs text-secondary mb-0">{{$enroll->amount}} -mmk</p>
+                      </td>
+
+                      <td class="align-middle text-center text-sm">
+                        <span class="badge badge-sm"><a href="{{$enroll -> image}}">Click to Download Slip</a></span>
                         
-                      @if($enroll->status == 1)
-                                              <span class="text-info ms-sm-2 font-weight-bold">  Pending </span>
-                                                @elseif($enroll->status == 2)
-                                               <span class="text-success ms-sm-2 font-weight-bold"> Approve</span>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <!-- <span class="badge badge-sm bg-gradient-success">Online</span> -->
+                        @if($enroll->status == 2)
+                                              <span class="badge badge-sm bg-gradient-success">  Approve </span>
+                                                @elseif($enroll->status == 1)
+                                               <span class="badge badge-sm bg-gradient-secondary"> Pending</span>
                                                @else
-                                               <span class="text-danger ms-sm-2 font-weight-bold"> Reject</span>
+                                               <span class="badge badge-sm bg-gradient-danger"> Rejected</span>
                                                 @endif
-                                              
-                    </span></span>
-                    <span class="mb-2 text-xs">Enrollment Slip: <span class="text-dark font-weight-bold ms-sm-2"><a href="{{$enroll -> image}}">Click to Download</a></span></span>
-                    <!-- a thit -->
-                  </div>
-                  @if($enroll->status==1)
-                  <div class="ms-auto text-end">
-                    <a class="btn btn-link text-danger text-gradient px-3 mb-0" onclick="return myFunction1();" href='/admin/enrollment/{{ $enroll->id }}/reject'><i class="material-icons text-sm me-2">cancel</i>Reject</a>
+                      </td>
+                      @if($enroll->status==1)
+                      <td class="align-middle text-center text-sm">
+                      
+                        <!-- <span class="badge badge-sm bg-gradient-success">Online</span> -->
+                        <a class="btn btn-link text-danger text-gradient px-3 mb-0" onclick="return myFunction1();" href='/admin/enrollment/{{ $enroll->id }}/reject'><i class="material-icons text-sm me-2">cancel</i>Reject</a>
 
                     <a class="btn btn-link text-dark px-3 mb-0" onclick="return myFunction();" href='/admin/enrollment/{{ $enroll->id }}/approve'><i class="material-icons text-sm me-2">check</i>Approve</a>
-                  </div>
-                  @else
-                  @endif
-                </li>
-                
-                </div>
-                
-                </div>
-                <br>
-            
-                </div>
-                
-            @endforeach
-            
-        </div>
-              {{ $enrolls->links() }}
+                      </td>
+                      @else
+                      @endif
 
+                      
+                      <!-- <td class="align-middle">
+                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                          Edit
+                        </a>
+                      </td> -->
+                    </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-      </div>
+
           <script>
                 function myFunction() {
                     if(!confirm("Are You Sure to update this ?"))
